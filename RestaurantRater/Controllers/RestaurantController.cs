707fe57fe37fd0e2bd207b0c38b080619ec02242
Tim <timculpcreations@gmail.com>
2020-08-24 15:06:04 -0400

    Add Create Method

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,33 @@ using System.Web.Mvc;
 
 namespace RestaurantRater.Controllers
 {
+
     public class RestaurantController : Controller
     {
+        private RestaurantDbContext _db = new RestaurantDbContext();
         // GET: Restaurant/Index
         public ActionResult Index()
         {
+            return View(_db.Restaurants.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant re)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(re);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(re);
         }
     }
 }
