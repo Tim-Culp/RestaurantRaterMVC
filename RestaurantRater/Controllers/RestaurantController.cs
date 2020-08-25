66b5b58c4ebf5372e94bffe77544e725a93e835d
Tim <timculpcreations@gmail.com>
2020-08-25 11:01:16 -0400

    add delete functionality

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -35,5 +35,28 @@ namespace RestaurantRater.Controllers
             }
             return View(re);
         }
+        [HttpGet]
+        public ActionResult Delete(int? ID)
+        {
+            if(ID==null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(ID);
+            if(restaurant == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+            }
+            return View(restaurant);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int ID)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(ID);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
